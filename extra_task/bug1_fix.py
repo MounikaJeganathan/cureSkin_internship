6d731fc20c7a73fc133cc773334e8443a6f08e0f
Mounika Jeganathan <mounika.jothi@gmail.com>
2023-07-17 11:40:15 -0700

    Updating the extra task 1 and fixed the bugs

--- extra_task/bug1_fix.py
+++ extra_task/bug1_fix.py
@@ -12,14 +12,15 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 browser = webdriver.Chrome()
-
+browser.implicitly_wait(5)
 url = "https://www.wikipedia.org/"
 browser.get(url)
 
-search_bar = browser.find_element(By.CSS_SELECTOR, "input.searchInput")
-search_query = "python"
+search_bar = browser.find_element(By.CSS_SELECTOR, "input#searchInput")
+search_query = "Python"
 search_bar.clear()
 search_bar.send_keys(search_query)
+browser.find_element(By.CSS_SELECTOR, "i.sprite.svg-search-icon").click()
 
 assert search_query in browser.current_url, f"Search query {search_query} not in {browser.current_url}"
 print("Test case passed")
